1b62b81d3d20d24c3f1d2da8bac8f3ab48d93422
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2020-05-28 13:39:40 +0200

    style: run formatter

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 if (!defined('ABSPATH')) {
-	// Exit if accessed directly.
-	exit; 
+    // Exit if accessed directly.
+    exit;
 }
 
 define('NETLIFY_BUILD_HOOK', getenv('NETLIFY_BUILD_HOOK'));
@@ -12,7 +12,7 @@ define('NETLIFY_BUILD_HOOK', getenv('NETLIFY_BUILD_HOOK'));
  */
 function load_scripts()
 {
-	wp_enqueue_style('smartwatch-style', get_stylesheet_uri());
+    wp_enqueue_style('smartwatch-style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'load_scripts');
@@ -22,7 +22,7 @@ add_action('wp_enqueue_scripts', 'load_scripts');
  */
 function remove_wordpress_version()
 {
-	return '';
+    return '';
 }
 
 add_filter('the_generator', 'remove_wordpress_version');
@@ -33,11 +33,11 @@ remove_action('wp_head', 'wp_generator');
  */
 function remove_core_updates()
 {
-	global $wp_version;
-	return (object)array(
-		'last_checked' => time() ,
-		'version_checked' => $wp_version,
-	);
+    global $wp_version;
+    return (object) array(
+        'last_checked'    => time(),
+        'version_checked' => $wp_version,
+    );
 }
 
 add_filter('pre_site_transient_update_core', 'remove_core_updates');
@@ -49,25 +49,23 @@ add_filter('pre_site_transient_update_themes', 'remove_core_updates');
  */
 function remove_version_from_files($src)
 {
-	if (strpos($src, 'ver=' . get_bloginfo('version')))
-	{
-		$src = remove_query_arg('ver', $src);
-	}
-	return $src;
+    if (strpos($src, 'ver=' . get_bloginfo('version'))) {
+        $src = remove_query_arg('ver', $src);
+    }
+    return $src;
 }
 
 add_filter('style_loader_src', 'remove_version_from_files');
 add_filter('script_loader_src', 'remove_version_from_files');
 add_filter('use_block_editor_for_post', '__return_false', 10);
 
-
 /**
  * Remove image sizes from image on edior
  */
 function remove_size_attributes($html)
 {
-	$html = preg_replace('/(width|height)="\d*"\s/', '', $html);
-	return $html;
+    $html = preg_replace('/(width|height)="\d*"\s/', '', $html);
+    return $html;
 }
 
 add_filter('post_thumbnail_html', 'remove_size_attributes', 10);
@@ -79,11 +77,11 @@ add_filter('image_send_to_editor', 'remove_size_attributes', 10);
  */
 function prefix_remove_default_images($sizes)
 {
-	unset($sizes['small']); // 150px
-	unset($sizes['medium']); // 300px
-	unset($sizes['medium_large']); // 768px
-	unset($sizes['large']); // 1024px
-	return $sizes;
+    unset($sizes['small']); // 150px
+    unset($sizes['medium']); // 300px
+    unset($sizes['medium_large']); // 768px
+    unset($sizes['large']); // 1024px
+    return $sizes;
 }
 
 add_filter('intermediate_image_sizes_advanced', 'prefix_remove_default_images');
@@ -94,7 +92,7 @@ add_filter('max_srcset_image_width', create_function('', 'return 1;'));
  */
 function image_tag_class($class, $id, $align, $size)
 {
-	return 'align' . $align . ' size-' . $size;
+    return 'align' . $align . ' size-' . $size;
 }
 add_filter('get_image_tag_class', 'image_tag_class', 0, 4);
 
@@ -103,87 +101,85 @@ add_filter('get_image_tag_class', 'image_tag_class', 0, 4);
  */
 function register_custom_post_types()
 {
-	register_post_type('product', array(
-		'labels' => array(
-			'name' => __('Products') ,
-			'singular_name' => __('Product')
-		) ,
-		'public' => true,
-		'has_archive' => true,
-		'rest_base' => 'products',
-		'graphql_single_name' => 'Product',
-		'graphql_plural_name' => 'Products',
-		'public' => true,
-		'show_in_rest' => true,
-		'show_in_graphql' => true,
-		'supports' => array(
-			'title'
-		) ,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'show_in_nav_menus' => true,
-		'show_in_admin_bar' => true,
-		'hierarchical' => true,
-		'menu_icon' => 'dashicons-products',
-	));
-	register_post_type('brand', array(
-		'labels' => array(
-			'name' => __('Brands') ,
-			'singular_name' => __('Brand')
-		) ,
-		'public' => true,
-		'has_archive' => true,
-		'rest_base' => 'brands',
-		'graphql_single_name' => 'Brand',
-		'graphql_plural_name' => 'Brands',
-		'public' => true,
-		'show_in_rest' => true,
-		'show_in_graphql' => true,
-		'supports' => array(
-			'title'
-		) ,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'show_in_nav_menus' => true,
-		'show_in_admin_bar' => true,
-		'hierarchical' => true,
-		'menu_icon' => 'dashicons-admin-page',
-	));
+    register_post_type('product', array(
+        'labels'              => array(
+            'name'          => __('Products'),
+            'singular_name' => __('Product'),
+        ),
+        'public'              => true,
+        'has_archive'         => true,
+        'rest_base'           => 'products',
+        'graphql_single_name' => 'Product',
+        'graphql_plural_name' => 'Products',
+        'public'              => true,
+        'show_in_rest'        => true,
+        'show_in_graphql'     => true,
+        'supports'            => array(
+            'title',
+        ),
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'hierarchical'        => true,
+        'menu_icon'           => 'dashicons-products',
+    ));
+    register_post_type('brand', array(
+        'labels'              => array(
+            'name'          => __('Brands'),
+            'singular_name' => __('Brand'),
+        ),
+        'public'              => true,
+        'has_archive'         => true,
+        'rest_base'           => 'brands',
+        'graphql_single_name' => 'Brand',
+        'graphql_plural_name' => 'Brands',
+        'public'              => true,
+        'show_in_rest'        => true,
+        'show_in_graphql'     => true,
+        'supports'            => array(
+            'title',
+        ),
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'hierarchical'        => true,
+        'menu_icon'           => 'dashicons-admin-page',
+    ));
 }
 
 add_action('init', 'register_custom_post_types');
-add_filter('acf/format_value', function ($value)
-{
-	if ($value instanceof WP_Post)
-	{
-		return ['post_type' => $value->post_type, 'id' => $value->ID, ];
-	}
+add_filter('acf/format_value', function ($value) {
+    if ($value instanceof WP_Post) {
+        return ['post_type' => $value->post_type, 'id' => $value->ID];
+    }
 
-	return $value;
+    return $value;
 }
-, 100);
+    , 100);
 
 /**
  * Disable remember me on login form
  */
 function remove_remember_me()
 {
-	unset($_POST['rememberme']);
+    unset($_POST['rememberme']);
 }
 
 function start_login_form_cache()
 {
-	ob_start('process_login_form_cache');
+    ob_start('process_login_form_cache');
 }
 
 function process_login_form_cache($content)
 {
-	$find = array(
-		'/<p class="forgetmenot">(.*)<\/p>/',
-		'/<p id="nav">(.*)<\/p>/',
-		'/<p id="backtoblog">(.*)<\/p>/'
-	);
-	return preg_replace($find, '', $content);
+    $find = array(
+        '/<p class="forgetmenot">(.*)<\/p>/',
+        '/<p id="nav">(.*)<\/p>/',
+        '/<p id="backtoblog">(.*)<\/p>/',
+    );
+    return preg_replace($find, '', $content);
 }
 
 add_action('login_form', 'start_login_form_cache');
@@ -197,7 +193,7 @@ add_filter('allow_password_reset', false);
  */
 function rest_url_prefix()
 {
-	return 'api';
+    return 'api';
 }
 
 add_filter('rest_url_prefix', 'rest_url_prefix');
@@ -207,23 +203,21 @@ add_filter('rest_url_prefix', 'rest_url_prefix');
  */
 function get_all_posts()
 {
-	$posts = get_posts(array(
-		'post_type' => 'post',
-		'posts_per_page' => 100,
-	));
+    $posts = get_posts(array(
+        'post_type'      => 'post',
+        'posts_per_page' => 100,
+    ));
 
-	if (empty($posts))
-	{
-		return null;
-	}
+    if (empty($posts)) {
+        return null;
+    }
 
-	foreach ($posts as $key => $post)
-	{
-		$post->custom_fields = get_fields($post->ID);
-		$post->categories = get_categories($post->ID);
-	}
+    foreach ($posts as $key => $post) {
+        $post->custom_fields = get_fields($post->ID);
+        $post->categories    = get_categories($post->ID);
+    }
 
-	return $posts;
+    return $posts;
 }
 
 /**
@@ -231,17 +225,16 @@ function get_all_posts()
  */
 function get_post_by_id($data)
 {
-	$post = get_post($data['id']);
+    $post = get_post($data['id']);
 
-	if (empty($post))
-	{
-		return null;
-	}
+    if (empty($post)) {
+        return null;
+    }
 
-	$post->custom_fields = get_fields($data['id']);
-	$post->categories = get_categories($data['id']);
+    $post->custom_fields = get_fields($data['id']);
+    $post->categories    = get_categories($data['id']);
 
-	return $post;
+    return $post;
 }
 
 /**
@@ -249,45 +242,42 @@ function get_post_by_id($data)
  */
 function get_all_products()
 {
-	$products = get_posts(array(
-		'post_type' => 'product',
-		'posts_per_page' => 100,
-	));
+    $products = get_posts(array(
+        'post_type'      => 'product',
+        'posts_per_page' => 100,
+    ));
 
-	if (empty($products))
-	{
-		return null;
-	}
+    if (empty($products)) {
+        return null;
+    }
 
-	foreach ($products as $key => $product)
-	{
-		$product->custom_fields = get_fields($product->ID);
-		$product->custom_fields['brand'] = get_post(get_field('brand', $product->ID) ['id']);
-		$product->custom_fields['amount'] = get_field('price', $product->ID);
-		$product->custom_fields['currency'] = 'EUR';
-	}
+    foreach ($products as $key => $product) {
+        $product->custom_fields             = get_fields($product->ID);
+        $product->custom_fields['brand']    = get_post(get_field('brand', $product->ID)['id']);
+        $product->custom_fields['amount']   = get_field('price', $product->ID);
+        $product->custom_fields['currency'] = 'EUR';
+    }
 
-	return $products;
+    return $products;
 }
 
 /**
- * Get product by ID 
+ * Get product by ID
  */
 function get_product_by_id($data)
 {
-	$product = get_post($data['id']);
+    $product = get_post($data['id']);
 
-	if (empty($product))
-	{
-		return null;
-	}
+    if (empty($product)) {
+        return null;
+    }
 
-	$product->custom_fields = get_fields($data['id']);
-	$product->custom_fields['brand'] = get_post(get_field('brand', $data['id']) ['id']);
-	$product->custom_fields['amount'] = get_field('price', $data['id']);
-	$product->custom_fields['currency'] = 'EUR';
+    $product->custom_fields             = get_fields($data['id']);
+    $product->custom_fields['brand']    = get_post(get_field('brand', $data['id'])['id']);
+    $product->custom_fields['amount']   = get_field('price', $data['id']);
+    $product->custom_fields['currency'] = 'EUR';
 
-	return $product;
+    return $product;
 }
 
 /**
@@ -295,25 +285,25 @@ function get_product_by_id($data)
  */
 function register_custom_rest_routes()
 {
-	register_rest_route('custom/v1', '/posts/', array(
-		'methods' => 'GET',
-		'callback' => 'get_all_posts',
-	));
+    register_rest_route('custom/v1', '/posts/', array(
+        'methods'  => 'GET',
+        'callback' => 'get_all_posts',
+    ));
 
-	register_rest_route('custom/v1', '/posts/(?P<id>\d+)', array(
-		'methods' => 'GET',
-		'callback' => 'get_post_by_id',
-	));
+    register_rest_route('custom/v1', '/posts/(?P<id>\d+)', array(
+        'methods'  => 'GET',
+        'callback' => 'get_post_by_id',
+    ));
 
-	register_rest_route('custom/v1', '/products/', array(
-		'methods' => 'GET',
-		'callback' => 'get_all_products',
-	));
+    register_rest_route('custom/v1', '/products/', array(
+        'methods'  => 'GET',
+        'callback' => 'get_all_products',
+    ));
 
-	register_rest_route('custom/v1', '/products/(?P<id>\d+)', array(
-		'methods' => 'GET',
-		'callback' => 'get_product_by_id',
-	));
+    register_rest_route('custom/v1', '/products/(?P<id>\d+)', array(
+        'methods'  => 'GET',
+        'callback' => 'get_product_by_id',
+    ));
 }
 
 add_action('rest_api_init', 'register_custom_rest_routes');
@@ -323,19 +313,17 @@ add_action('rest_api_init', 'register_custom_rest_routes');
  */
 function remove_default_endpoints($endpoints)
 {
-	$routes = array();
-	foreach ($endpoints as $key => $endpoint)
-	{
-		$namespace_endpoint = "custom/v1";
+    $routes = array();
+    foreach ($endpoints as $key => $endpoint) {
+        $namespace_endpoint = "custom/v1";
 
-		if ($endpoint["namespace"] == $namespace_endpoint)
-		{
-			$routes[$key] = $endpoints[$key];
-		}
-	}
+        if ($endpoint["namespace"] == $namespace_endpoint) {
+            $routes[$key] = $endpoints[$key];
+        }
+    }
 
-	$endpoints = $routes;
-	return (array)$endpoints;
+    $endpoints = $routes;
+    return (array) $endpoints;
 }
 
 add_filter('rest_endpoints', 'remove_default_endpoints');
--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
 
 if (!defined('ABSPATH')) {
-	// Exit if accessed directly.
-	exit; 
+    // Exit if accessed directly.
+    exit;
 }
 
-header( 'HTTP/1.1 404 Not Found' );
-header( 'Content-Type: application/json' );
-
+header('HTTP/1.1 404 Not Found');
+header('Content-Type: application/json');
 
 echo json_encode(
-	array(
-		'code'    => 'rest_no_route',
-		'message' => 'No route was found matching the URL and request method',
-		'data'    => (object) array('status' => 404)
-	)
+    array(
+        'code'    => 'rest_no_route',
+        'message' => 'No route was found matching the URL and request method',
+        'data'    => (object) array('status' => 404),
+    )
 );
 
 die;
-
